18c854152f373e0580b56cce10440d5dfd1e96d0
ismat-samadov <ismetsemedov@gmail.com>
2025-12-14 21:12:01 +0400

    3rd place

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-SOCAR Historical Document Processing System
-
-A complete solution for transforming historical handwritten and printed documents
-into an interactive, searchable knowledge base.
-"""
-
-__version__ = "1.0.0"
